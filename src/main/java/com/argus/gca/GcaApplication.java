de3416cf6495157a12258f240533c9023266f5bd
Arvind-Sabarinathan <arvind.personal2004@gmail.com>
2025-10-19 20:24:37 +0530

    Initial commit

---
+++ src/main/java/com/argus/gca/GcaApplication.java
@@ -0,0 +1,13 @@
+package com.argus.gca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/argus/gca/GcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argus.gca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
